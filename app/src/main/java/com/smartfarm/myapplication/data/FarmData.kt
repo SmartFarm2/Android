a7533f6ba11c070dd75785d4f3f861b81181e26f
munyoung03 <lmun768@naver.com>
2021-01-14 16:31:13 +0900

    [UPDATE] main

--- app/src/main/java/com/smartfarm/myapplication/data/FarmData.kt
+++ app/src/main/java/com/smartfarm/myapplication/data/FarmData.kt
@@ -1,7 +1,9 @@
 package com.smartfarm.myapplication.data
 
+import com.smartfarm.myapplication.application.MyApp
+
 data class FarmData(
     val _name : String
 ) {
-    val name : String get()=_name + "의 농장"
+    val name : String get()= MyApp.pref.name + "의 농장"
 }
