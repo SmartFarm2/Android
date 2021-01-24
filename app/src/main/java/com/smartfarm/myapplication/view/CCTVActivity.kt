e32a9fcac40c874f50666083deee5980d6011441
munyoung03 <lmun768@naver.com>
2021-01-25 04:46:36 +0900

    asdf

--- app/src/main/java/com/smartfarm/myapplication/view/CCTVActivity.kt
+++ app/src/main/java/com/smartfarm/myapplication/view/CCTVActivity.kt
@@ -18,14 +18,10 @@ import com.google.common.net.HttpHeaders
 import com.smartfarm.myapplication.application.MyApp
 import com.smartfarm.myapplication.data.Constants
 import com.smartfarm.myapplication.network.SocketManager
-import com.smartfarm.myapplication.viewmodel.CCTVActivityViewModel
-import com.smartfarm.myapplication.viewmodel.CCTVActivityViewModelFactory
 
 class CCTVActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCctvBinding
-    private lateinit var viewModel: CCTVActivityViewModel
-    private lateinit var viewModelFactory: CCTVActivityViewModelFactory
 
     private lateinit var playerView: PlayerView
     private var player: SimpleExoPlayer? = null
@@ -53,20 +49,15 @@ class CCTVActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         manager = SocketManager.getInstance(application)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_cctv)
-        viewModelFactory = CCTVActivityViewModelFactory(20, application)
-        viewModel = ViewModelProvider(this, viewModelFactory).get(CCTVActivityViewModel::class.java)
 
         manager.emit(Constants.CCTV, true)
 
         with(binding) {
-            myViewModel = viewModel
             lifecycleOwner = this@CCTVActivity
             playerView = playerV
         }
 
-        with(viewModel){
-            observing()
-        }
+
     }
 
     private fun initializePlayer() {
@@ -96,7 +87,6 @@ class CCTVActivity : AppCompatActivity() {
     }
 
     override fun onDestroy() {
-        viewModel.deobserving()
         super.onDestroy()
     }
 }
